b78b9dbcfb2ef9ba9208b1edd1f0f7fe77c5bf32
nate foss <70177794+nathanielfoss11@users.noreply.github.com>
2021-02-26 15:49:09 -0700

    Initial Repo Setup

---
+++ client/src/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    
+    render() {
+      return ( 
+        <div>
+          <h1>Nathaniel Foss</h1>
+        </div>
+      );
+    }
+}
+
+export default Homepage;
